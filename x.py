f85fae35788a708a8a4f16ee7862ba0082007e48
Gyanbardhan <120886311+Gyanbardhan@users.noreply.github.com>
2024-05-29 11:33:53 +0530

    Update x.py

--- x.py
+++ x.py
@@ -19,7 +19,6 @@ if not os.path.exists(nltk_data_dir):
 nltk.data.path.clear()
 nltk.data.path.append(nltk_data_dir)
 nltk.download("stopwords", download_dir=nltk_data_dir)
-nltk.download('punkt', download_dir=nltk_data_dir)
 from nltk.corpus import stopwords
 sw_list=stopwords.words('english')
 
