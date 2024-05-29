4d851e0c2ad38b76ac77eade8155c9bb7774963b
Gyanbardhan <120886311+Gyanbardhan@users.noreply.github.com>
2024-05-29 11:31:42 +0530

    Update x.py

--- x.py
+++ x.py
@@ -12,7 +12,14 @@ def lwr(text):
     return text.lower()
     
 import nltk
-nltk.download()
+
+nltk_data_dir = "./resources/nltk_data_dir/"
+if not os.path.exists(nltk_data_dir):
+    os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.data.path.clear()
+nltk.data.path.append(nltk_data_dir)
+nltk.download("stopwords", download_dir=nltk_data_dir)
+nltk.download('punkt', download_dir=nltk_data_dir)
 from nltk.corpus import stopwords
 sw_list=stopwords.words('english')
 
